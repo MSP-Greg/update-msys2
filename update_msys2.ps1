1075406fa094c0a0fba5a1746553ab08807f015c
MSP-Greg <Greg.mpls@gmail.com>
2020-06-06 10:10:05 -0500

    Add matrix 64 and 32

--- update_msys2.ps1
+++ update_msys2.ps1
@@ -4,11 +4,20 @@
 
 $env:Path = "C:/msys64/usr/bin;$env:Path"
 $conf     = "C:/msys64/etc/pacman.conf"
+$args     = '--noconfirm', '--needed', '--noprogressbar'
 
+if ($env:MSYSTEM -match 'mingw32') {
+  $pre = 'mingw-w64-i686'
+} elseif ($env:MSYSTEM -match 'mingw64') {
+  $pre = 'mingw-w64-x86_64'
+} else {
+  echo Incorrect MSYSTEM value!
+  exit 1
+}
+
+# stop pacman disk space checks
 $raw = $(Get-Content -Path $conf -Raw) -replace '(?m)^CheckSpace', '#CheckSpace'
 Out-File -FilePath $conf -InputObject $raw
-
-$args = '--noconfirm', '--needed', '--noprogressbar'
 
 $msys_pkgs = 'msys2-runtime', 'pacman'
 pacman.exe -Sy $args $msys_pkgs
@@ -21,4 +30,4 @@ $msys_pkgs = 'base', 'base-devel', 'compression'
 Write-Host "pacman.exe -S $args $msys_pkgs"
 pacman.exe -S $args $msys_pkgs
 
-pacman.exe -S $args mingw-w64-x86_64-toolchain
+pacman.exe -S $args $pre-toolchain
