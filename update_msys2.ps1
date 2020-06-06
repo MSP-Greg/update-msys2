4f0a6181ee71b6d39f699e20a3ca95bbc84ead57
MSP-Greg <Greg.mpls@gmail.com>
2020-06-05 22:59:52 -0500

    Update update_msys2.ps1

--- update_msys2.ps1
+++ update_msys2.ps1
@@ -1,8 +1,9 @@
 <#
 
 #>
+
 $env:Path = "C:/msys64/usr/bin;$env:Path"
-$conf = "C:/msys64/etc/pacman.conf"
+$conf     = "C:/msys64/etc/pacman.conf"
 
 $raw = $(Get-Content -Path $conf -Raw) -replace '(?m)^CheckSpace', '#CheckSpace'
 Out-File -FilePath $conf -InputObject $raw
@@ -10,13 +11,14 @@ Out-File -FilePath $conf -InputObject $raw
 $args = '--noconfirm', '--needed', '--noprogressbar'
 
 $msys_pkgs = 'msys2-runtime', 'pacman'
-pacman.exe -Sy $args msys2-runtime pacman
+pacman.exe -Sy $args $msys_pkgs
 taskkill /f /fi "MODULES eq msys-2.0.dll"
 
-pacman.exe -Sy $args msys2-runtime pacman
+pacman.exe -Sy $args $msys_pkgs
 taskkill /f /fi "MODULES eq msys-2.0.dll"
 
 $msys_pkgs = 'base', 'base-devel', 'compression'
+Write-Host "pacman.exe -S $args $msys_pkgs"
 pacman.exe -S $args $msys_pkgs
 
 pacman.exe -S $args mingw-w64-x86_64-toolchain
